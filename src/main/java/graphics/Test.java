a6241132917aa0d0af6cac3ea03c26f891c28022
paras <parasad612@gmail.com>
2023-10-03 20:41:07 +0200

    changed pom xml and graphics test files

--- src/main/java/graphics/Test.java
+++ src/main/java/graphics/Test.java
@@ -1,18 +1,17 @@
 package graphics;
 
-import org.antlr.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import java.awt.*;
 import java.io.IOException;
 
-public class Main {
+public class Test {
     public static void main(String[] args) throws IOException {
         CharStream input = CharStreams.fromFileName("./src/main/java/graphics/test.txt");
         GraphicsLexer lexer = new GraphicsLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         GraphicsParser parser = new GraphicsParser(tokens);
+        parser.file();
     }
 }
--- src/main/java/hello/Test.java
+++ src/main/java/hello/Test.java
@@ -5,7 +5,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
 
-public class Main {
+public class Test {
     public static void main(String[] args) throws IOException {
         CharStream a = CharStreams.fromFileName("./src/main/java/hello/test.txt");
         HelloLexer lexer = new HelloLexer(a);
