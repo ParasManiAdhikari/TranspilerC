36dd8dfe02e0129f015d442185119f71ebe093f6
paras <parasad612@gmail.com>
2024-01-09 20:25:59 +0100

    1 finished 3 started

--- src/test/java/dsl/InterneTest.java
+++ src/test/java/dsl/InterneTest.java
@@ -1,27 +1,8 @@
 package dsl;
 
-import DSL.CSVLexer;
-import DSL.CSVParser;
 import DSL.Interne.AFSMAutomat;
 import DSL.Interne.AFSMInterpreter;
-import DSL.Services.CSVLoader;
-import DSL.Services.LexAnalysator;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.junit.Assert;
-import org.junit.experimental.theories.DataPoint;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
