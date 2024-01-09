46f4d9ac8ed27cbb567b56b7d27cb14c5c858ee3
paras <parasad612@gmail.com>
2024-01-09 23:32:40 +0100

    aufgabe 3 gemacht 2

--- src/main/java/DSL/Services/LexAnalysator.java
+++ src/main/java/DSL/Services/LexAnalysator.java
@@ -12,14 +12,7 @@ public class LexAnalysator {
     public String automat(HashMap<String[], HashMap<String, String>> table) {
         String[] state = {"",""};
         state[0] = table.keySet().stream().filter(x -> x[1].equals("_start")).collect(Collectors.toList()).get(0)[0];
-        for(int i = 0; i < table.size(); i++){
-            String copy = state[0];
-            List<String[]> a = table.keySet().stream().filter(x -> x[0].equals(state[0])).collect(Collectors.toList());
-            String text = a.get(0)[1];
-            boolean test = text == "_end";
-            if(test){
-                break;
-            }
+        for(int i = 1; i < table.size(); i++){
             state[0] = table.entrySet()
                     .stream()
                     .filter(x-> x.getKey()[0].equals(state[0]))
